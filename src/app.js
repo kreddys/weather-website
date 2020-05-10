510984800207744a54e04f788bb9717da9088b3c
Kishore Reddy <kishore5214@outlook.com>
2020-05-09 20:52:41 -0500

    Setup app for heroku

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ const forecast = require('./utils/forecast')
 
 const app = express()
 
+const port = process.env.PORT || 3000
+
 
 //Define paths for express config
 const publicDirPath = (path.join(__dirname,'../','public'))
@@ -82,6 +84,6 @@ app.get('*', (req,res) => {
     })
 })
 
-app.listen(3000,() => {
-    console.log('Sever is up on port 3000')
+app.listen(port,() => {
+    console.log('Sever is up on port', port)
 })
